21f7e3b66fd48522658b0fe0fb590db21327b874
Charlie <root.nishi@gmail.com>
2019-09-02 18:01:55 +0530

    Add Infix to Postfix Translator.

---
+++ src/main/java/com/somedomain/algos/expressions/InfixToPostfixConverter.java
@@ -0,0 +1,75 @@
+package com.somedomain.algos.expressions;
+
+import com.somedomain.ds.containers.stack.ArrayBackedStack;
+import com.google.common.flogger.FluentLogger;
+
+import java.util.Arrays;
+
+public class InfixToPostfixConverter {
+    //private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+    public static String convertToPostfix(final String infixExpression, final char delimiter) {
+        StringBuilder targetPostfixExpression = new StringBuilder("");
+
+        if (infixExpression != null && infixExpression.length() > 3) {
+            String[] tokens = infixExpression.trim().split(String.valueOf(delimiter));
+            //logger.atFinest().log("Received Tokens: %s", Arrays.asList(tokens));
+            int numberOfTokens = tokens.length;
+            ArrayBackedStack<String> operandStack = new ArrayBackedStack<>(String.class, numberOfTokens);
+            int tokenPointer = 0;
+            while (tokenPointer < numberOfTokens) {
+                String token = tokens[tokenPointer];
+                char firstCharOfToken = token.charAt(0);
+                switch (firstCharOfToken) {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': {
+                        targetPostfixExpression.append(token).append(delimiter);
+                        break;
+                    }
+                    case '-':
+                    case '+':
+                    case '*':
+                    case '/':
+                    case '^': {
+                        int priority = MathsOperators.operatorFor(firstCharOfToken).getPriority();
+                        String nextInStack = operandStack.peek();
+                        int nextOperandPriority = nextInStack == null ? -1 : MathsOperators.operatorFor(nextInStack.charAt(0)).getPriority();
+                        while (nextInStack != null && !nextInStack.equals("(") && nextOperandPriority > priority) {
+                            targetPostfixExpression.append(nextInStack).append(delimiter);
+                            operandStack.pop(); // Remove current from Stack.
+                            nextInStack = operandStack.peek();
+                            nextOperandPriority = nextInStack == null ? -1 : MathsOperators.operatorFor(nextInStack.charAt(0)).getPriority();
+                        }
+                        operandStack.push(token);
+                        break;
+                    }
+                    case '(': {
+                        operandStack.push(token);
+                        break;
+                    }
+                    case ')': {
+                        String nextInStack = operandStack.pop();
+                        while (!nextInStack.equals("(")) {
+                            targetPostfixExpression.append(nextInStack).append(delimiter);
+                            nextInStack = operandStack.pop();
+                        }
+                        break;
+                    }
+
+                }
+                tokenPointer++;
+            }
+            targetPostfixExpression.append(operandStack.pop());
+        }
+        return targetPostfixExpression.toString();
+
+    }
+}
