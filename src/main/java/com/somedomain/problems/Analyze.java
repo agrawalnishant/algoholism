dd9fa68f5e9cf274a0f2cf7aec2e4d10b1f5dabd
Charlie <root.nishi@gmail.com>
2019-11-10 10:46:50 +0530

    Add check if String is permutation of another.

--- src/main/java/com/somedomain/problems/Analyze.java
+++ src/main/java/com/somedomain/problems/Analyze.java
@@ -4,11 +4,14 @@ public class Analyze {
 
     private static final String TEST_STRING_UNIQ = "ABCDEF";
     private static final String TEST_STRING_REPEAT = "ABCDEFABC";
+    private static final String TEST_STRING_PERMUTATION_OF_UNIQ = "ABEDC";
 
     public static void main(String[] args) {
         System.out.println(TEST_STRING_REPEAT + " has uniq?: " + hasAllUniqueChars(TEST_STRING_REPEAT));
         System.out.println(TEST_STRING_UNIQ + " has uniq?: " + hasAllUniqueChars(TEST_STRING_UNIQ));
         System.out.println(TEST_STRING_REPEAT + " : reversed : " + reverseInplace(TEST_STRING_REPEAT));
+        System.out.println("Is " + "ABCBDEF" + " permutation of " + "ABCDEFB" + " : "
+                + arePermutations(TEST_STRING_UNIQ, TEST_STRING_PERMUTATION_OF_UNIQ));
     }
 
     private static String reverseInplace(final String sourceString) {
@@ -50,6 +53,28 @@ public class Analyze {
         }
 
         return hasAllUnique;
+    }
+
+    private static boolean arePermutations(final String sourceString, final String targetString) {
+        if (sourceString == targetString) {
+            return true;
+        }
+        if (null != targetString && null != sourceString && sourceString.equals(targetString)) {
+            return true;
+        }
+        int charsetSize = 256;
+
+        int lengthOfSource = sourceString.length();
+        int[] sourceDistri = new int[charsetSize];
+        for (int c : sourceString.toCharArray()) {
+            sourceDistri[c]++;
+        }
+        for (char c : targetString.toCharArray()) {
+            if (sourceDistri[c]-- <= 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
