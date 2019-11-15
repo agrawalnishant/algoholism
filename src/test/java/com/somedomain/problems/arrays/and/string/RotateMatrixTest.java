70e307d0de68df8000d5da97b00a9ec615a45f91
Charlie <root.nishi@gmail.com>
2019-11-15 09:08:47 +0530

    Refactor matrix rotation and its test.

--- src/test/java/com/somedomain/problems/arrays/and/string/RotateMatrixTest.java
+++ src/test/java/com/somedomain/problems/arrays/and/string/RotateMatrixTest.java
@@ -2,8 +2,6 @@ package com.somedomain.problems.arrays.and.string;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class RotateMatrixTest {
@@ -11,13 +9,32 @@ class RotateMatrixTest {
     @Test
     void rotateMatrixClockwise() {
         int[][] sourceMatrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[][] expectedMatrix = {{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}};
         RotateMatrix rotateMatrix = new RotateMatrix();
-        print2D(sourceMatrix);
         rotateMatrix.rotateMatrixClockwise(sourceMatrix);
-        print2D(sourceMatrix);
+        assertTrue(compareMatrices(expectedMatrix, sourceMatrix), () -> "Rotated Matrix should be " + print2D(expectedMatrix))
+        ;
+
     }
 
-    public static void print2D(int mat[][]) {
+    private static boolean compareMatrices(int[][] matrixOne, int[][] matrixTwo) {
+
+        boolean result = false;
+        if (matrixOne.length == matrixTwo.length && matrixOne[0].length == matrixTwo[0].length) {
+            result = true;
+            for (int row = 0; result == true && row < matrixOne.length; row++) {
+                for (int col = 0; result == true && col < matrixOne[0].length; col++) {
+                    if (matrixOne[row][col] != matrixTwo[row][col]) {
+                        result = false;
+                    }
+                }
+            }
+        }
+        return result;
+
+    }
+
+    public static String print2D(int mat[][]) {
         // Loop through all rows
         for (int i = 0; i < mat.length; i++) {
             System.out.println("");
@@ -27,5 +44,6 @@ class RotateMatrixTest {
 
         }
         System.out.println("");
+        return "";
     }
 }
