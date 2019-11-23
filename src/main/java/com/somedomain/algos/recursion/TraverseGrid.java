0099db5e50a2fb51c48a94e0d1b78d3fb7a719b2
Charlie <root.nishi@gmail.com>
2019-11-23 16:13:25 +0530

    [Algo] Add exclusion to grid traversal.

--- src/main/java/com/somedomain/algos/recursion/TraverseGrid.java
+++ src/main/java/com/somedomain/algos/recursion/TraverseGrid.java
@@ -3,6 +3,7 @@ package com.somedomain.algos.recursion;
 import com.google.common.flogger.FluentLogger;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -13,13 +14,19 @@ public class TraverseGrid {
     private HashMap<Point, Integer> cache = new HashMap<Point, Integer>();
     private AtomicInteger iterations = new AtomicInteger(0);
 
-    public int traverse(final Point source, final Point destination) {
-        int numPaths = countPaths(source, destination);
+    public int traverse(final Point source, final Point destination, final ArrayList<Point> exclusions) {
+        if (source == null || destination == null) {
+            return 0;
+        }
+        if (source.equals(destination)) {
+            return 1;
+        }
+        int numPaths = countPaths(source, destination, exclusions);
         printCachedPaths();
         return numPaths;
     }
 
-    private int countPaths(final Point source, final Point dest) {
+    private int countPaths(final Point source, final Point dest, ArrayList<Point> exclusions) {
 
         Integer pathNum = cache.size() > 0 && cache.get(source) != null ? cache.get(source) : 0;
 
@@ -29,17 +36,17 @@ public class TraverseGrid {
             pathNum = 0;
         }
         iterations.getAndIncrement();
-        if (source.getX() < dest.getX()) {
+        if (source.getX() < dest.getX() && !exclusions.contains(source)) {
             Point nextPoint = new Point(source);
             nextPoint.translate(1, 0);
-            int count = countPaths(nextPoint, dest);
+            int count = countPaths(nextPoint, dest, exclusions);
             cache.put(nextPoint, count);
             pathNum += count;
         }
-        if (source.getY() < dest.getY()) {
+        if (source.getY() < dest.getY() && !exclusions.contains(source)) {
             Point nextPoint = new Point(source);
             nextPoint.translate(0, 1);
-            int count = countPaths(nextPoint, dest);
+            int count = countPaths(nextPoint, dest, exclusions);
             cache.put(nextPoint, count);
             pathNum += count;
         }
