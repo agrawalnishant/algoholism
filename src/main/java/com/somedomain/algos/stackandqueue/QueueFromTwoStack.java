945d20159f18618e80127bda9acef41b417030be
Nishant Agrawal <root.nishi@gmail.com>
2019-12-06 23:45:59 +0530

    Create a Queue from 2 Stacks.

---
+++ src/main/java/com/somedomain/algos/stackandqueue/QueueFromTwoStack.java
@@ -0,0 +1,67 @@
+package com.somedomain.algos.stackandqueue;
+
+import com.somedomain.ds.containers.queue.Queue;
+import com.somedomain.ds.containers.stack.ArrayBackedStack;
+import com.somedomain.ds.containers.stack.Stack;
+
+public class QueueFromTwoStack<T> implements Queue<T> {
+
+    private static final Integer DEFAULT_SIZE = 10;
+
+    private int size;
+    private Stack<T> stackHead;
+    private Stack<T> stackTail;
+
+    private Stack<T> createStack(Class<T> type) {
+        return new ArrayBackedStack<T>(type, DEFAULT_SIZE);
+    }
+
+    public QueueFromTwoStack(Class<T> type) {
+        this.stackHead = createStack(type);
+        this.stackTail = createStack(type);
+    }
+
+    @Override
+    public int queue(T element) {
+        if (!stackHead.isFull()) {
+            stackTail.push(element);
+            return ++size;
+        } else {
+            if (stackHead.isEmpty()) {
+                transferToHead();
+                stackTail.push(element);
+                return ++size;
+            } else {
+                return NO_HOPE;
+            }
+        }
+
+    }
+
+    private void transferToHead() {
+        while (!stackTail.isEmpty()) {
+            stackHead.push(stackTail.pop());
+
+        }
+    }
+
+    @Override
+    public T dequeue() {
+        if (!stackHead.isEmpty()) {
+            size--;
+            return stackHead.pop();
+        }
+        if (stackHead.isEmpty() && !stackTail.isEmpty()) {
+            transferToHead();
+            size--;
+            return stackHead.pop();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int count() {
+        return size;
+    }
+}
