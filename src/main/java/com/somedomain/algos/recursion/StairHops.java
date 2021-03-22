7d85f28c32b8725aafb4830f0b13c5b4c6b68628
agrawalnishant <root.nishi@gmail.com>
2021-03-23 03:05:07 +0530

    Count Staircase Hop by Memoization.

--- src/main/java/com/somedomain/algos/recursion/StairHops.java
+++ src/main/java/com/somedomain/algos/recursion/StairHops.java
@@ -2,17 +2,21 @@ package com.somedomain.algos.recursion;
 
 import com.google.common.flogger.FluentLogger;
 
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class StairHops {
 
     private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+    private static Integer[] hopsMap = null;
     private final AtomicLong counter = new AtomicLong();
-    private final Long[] hopsMap = new Long[100];
 
+    public StairHops(){
+        hopsMap = null;
+    }
 
-    public static long naiveHopscotch(int steps) {
-        long numWays = 0;
+    public static int naiveHopscotch(int steps) {
+        int numWays = 0;
         if (steps < 0) {
             numWays = 0;
         } else if (steps == 0) {
@@ -24,5 +28,24 @@ public class StairHops {
         return numWays;
     }
 
+    public static int memoizedHopscotch(int steps) {
+        int numWays = 0;
+        if (hopsMap == null) {
+            hopsMap = new Integer[steps + 1];
+            Arrays.fill(hopsMap, -1);
+        }
+        if (steps < 0) {
+            return 0;
+        } else if (steps == 0) {
+            return 1;
+        } else if (hopsMap[steps] != -1) {
+            return hopsMap[steps];
+        } else {
+            numWays = memoizedHopscotch(steps - 1) + memoizedHopscotch(steps - 2) + memoizedHopscotch(steps - 3);
+            hopsMap[steps] = numWays;
+            return numWays;
+        }
+
+    }
 
 }
